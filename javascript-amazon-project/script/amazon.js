d90abd6ca95da4b4b6c5d6ad8840f65ca6d0e1ba
Oasis <orisaekedavid@gmail.com>
2023-10-11 05:46:48 -0700

    Make add to cart button interactive

--- javascript-amazon-project/script/amazon.js
+++ javascript-amazon-project/script/amazon.js
@@ -49,7 +49,8 @@ products.forEach((product) => {
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id = "${product.id}">
                 Add to Cart
             </button>
             </div>
@@ -57,3 +58,30 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+//add to cart
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            const productId = button.dataset.productId;
+
+            //to check the cart for quantity and items
+            let matchingItem;
+            cart.forEach((item) => {
+                if (productId === item.productId) {
+                    matchingItem = item
+                }
+            });
+
+            if(matchingItem) {
+                matchingItem.quantity += 1;
+            } else {
+                cart.push({
+                    productId: productId,
+                    quantity: 1
+                });
+            }
+
+            console.log(cart);
+        });
+    });
