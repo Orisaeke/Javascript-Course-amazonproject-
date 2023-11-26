315d808beb29adc66f25de98e08a1a3cb0f3759f
oasis <oasisgroupsofcompany@gmail.com>
2023-11-26 05:09:04 -0800

    Organising code

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js'
 
 let productsHTML = '';
@@ -59,35 +59,43 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+const updateCartQuantity = () => {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  
+  document.querySelector('.js-cart-quantity')
+  .innerHTML = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
-
-      let matchingItem;
-
-      cart.forEach((item) => {
-        if (productId === item.productId) {
-          matchingItem = item;
-        }
-      });
-
-      if (matchingItem) {
-        matchingItem.quantity += 1;
-      } else {
-        cart.push({
-          productId: productId,
-          quantity: 1
-        });
-      }
-
-      let cartQuantity = 0;
-
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
-      });
-
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
+      addToCart(productId);
+      updateCartQuantity();
     });
   });
+
+  // To make the add to cart feauture a bit interactive
+  // document.querySelectorAll('.js-add-to-cart')
+  // .forEach((button) => {
+  //     button.addEventListener('click', () => {
+  //         const productId = button.dataset.productId;
+  
+  //         addToCart(productId);
+  //         updateCartQuantity();
+          
+  //     //added to cart symbol
+  //     const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+  //     addedMessage.classList.add('added-to-cart-visible');
+  
+  //     setTimeout(() => {
+  //         addedMessage.classList.remove('added-to-cart-visible');
+  //       }, 2000);
+  //     });
+  // });
+  
