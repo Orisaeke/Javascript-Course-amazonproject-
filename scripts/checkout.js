bb7749dc64ab1342e115e809b4c336d364aff6e7
Oasis <oasisgroupsofcompany@gmail.com>
2023-11-28 04:13:40 -0800

    checkout page(html generated with JS)

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,8 @@
 import {cart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
+let cartSummaryHTML = '';
+
 cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
@@ -12,9 +14,7 @@ cart.forEach((cartItem) => {
         }
     });
 
-    console.log(matchingProduct);
-
-    `
+    cartSummaryHTML += `
         <div class="cart-item-container">
         <div class="delivery-date">
         Delivery date: Tuesday, June 21
@@ -22,18 +22,18 @@ cart.forEach((cartItem) => {
 
         <div class="cart-item-details-grid">
         <img class="product-image"
-            src="images/products/athletic-cotton-socks-6-pairs.jpg">
+            src="${matchingProduct.image}">
 
         <div class="cart-item-details">
             <div class="product-name">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+            ${matchingProduct.name}
             </div>
             <div class="product-price">
-            $10.90
+            $${matchingProduct.priceCents / 100}
             </div>
             <div class="product-quantity">
             <span>
-                Quantity: <span class="quantity-label">2</span>
+                Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
             <span class="update-quantity-link link-primary">
                 Update
@@ -90,6 +90,7 @@ cart.forEach((cartItem) => {
         </div>
         </div>
     </div>
-
-    `
+    `;
 });
+
+const check = document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
