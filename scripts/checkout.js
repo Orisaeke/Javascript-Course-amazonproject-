d5f441ab7dcd9f33e035dfc444f8550eb5a42ac8
Oasis <oasisgroupsofcompany@gmail.com>
2023-11-30 11:02:12 -0800

    remove cart items v1.0

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -101,6 +101,7 @@ document.querySelectorAll('.js-delete-link')
     .forEach((link) => {
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
-            console.log(productId);
+            removeFromCart(productId);
+            console.log(cart);
         });
     });
