121af8cbb9c3bb674496146572f868879cc67093
Oasis <oasisgroupsofcompany@gmail.com>
2023-11-30 11:11:37 -0800

    remove cart items v2.0 (complete)

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,8 @@ cart.forEach((cartItem) => {
     });
 
     cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container
+         js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
         Delivery date: Tuesday, June 21
         </div>
@@ -97,11 +98,14 @@ cart.forEach((cartItem) => {
 
 const check = document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
+//deleting cart items from cart
 document.querySelectorAll('.js-delete-link')
     .forEach((link) => {
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
             removeFromCart(productId);
-            console.log(cart);
+            
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
         });
     });
